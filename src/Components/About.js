74855d14f05c3648796b573a612272d69e124fb2
Kala Elam <speechgirl1505@gmail.com>
2019-11-27 13:21:18 -0600

    style and pic changes

--- src/Components/About.js
+++ src/Components/About.js
@@ -7,7 +7,6 @@ class About extends Component {
       var name = this.props.data.name;
       var profilepic= "images/"+this.props.data.image;
       var bio = this.props.data.bio;
-      var street = this.props.data.address.street;
       var city = this.props.data.address.city;
       var state = this.props.data.address.state;
       var zip = this.props.data.address.zip;
@@ -24,16 +23,13 @@ class About extends Component {
          </div>
          <div className="nine columns main-col">
             <h2>About Me</h2>
-
             <p>{bio}</p>
             <div className="row">
                <div className="columns contact-details">
                   <h2>Contact Details</h2>
                   <p className="address">
 						   <span>{name}</span><br />
-						   <span>{street}<br />
 						         {city} {state} {zip}
-                   </span><br />
 						   <span>{phone}</span><br />
                      <span>{email}</span>
 					   </p>
