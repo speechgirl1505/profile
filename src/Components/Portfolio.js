5fcef81daea01dc26c5d3df3de87e24705932f61
Kala Elam <speechgirl1505@gmail.com>
2020-01-23 14:09:51 -0600

    added more infor and btns for projects

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 class Portfolio extends Component {
+
   render() {
 
     if(this.props.data){
@@ -12,14 +13,23 @@ class Portfolio extends Component {
                <img alt={projects.title} src={projectImage} />
                <div className="overlay">
                   <div className="portfolio-item-meta">
-                 <h5>{projects.title}</h5>
-                     <p>{projects.category}</p>
+                 <h5>Technologies Used:</h5>
+
+                     <p>{projects.technologies}</p>
                   </div>
                 </div>
-              <div className="link-icon"><i className="fa fa-link"></i></div>
+              {/* <div className="link-icon"><i className="fa fa-link"></i></div> */}
             </a>
+            {/* drop-icon <i class="fas fa-angle-down fa-3x"></i> */}
           </div>
-          <div className="drop-icon"><i class="fas fa-angle-down fa-3x"></i></div>
+          {/* <a className="drop-icon"><i class="fas fa-angle-down fa-3x"></i></a> */}
+          <div className="projInfo">
+            <h3 className="projTitle">{projects.title}</h3>
+            <p className="projDesc">{projects.category}</p>
+            {/* <p className="projtech"><strong>Technologies used:</strong> {projects.technologies}</p> */}
+            <a href={projects.url}><button  className="portBtn">Website</button></a>
+            <a href={projects.github}><button  className="portBtn">Github</button></a>
+            </div>
         </div>
       })
     }
